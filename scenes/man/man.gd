65d4c85e5a302c8df91ca071a0c0b697a28b31db
dandyhorse <dandyhorse@yandex.ru>
2023-11-05 13:01:00 +0400

    some changes, idk, still draft

--- scenes/man/man.gd
+++ scenes/man/man.gd
@@ -1,14 +1,22 @@
 extends CharacterBody2D
 
+
 @onready var cam = $PlayerCamera
+@onready var legs = $Rotator/AnimatedSprite2D
+@onready var rotator = $Rotator
 var realVelocity = Vector2.ZERO
 var zoom_step = 0.5
 
 const SPEED = 120.0
+const CROUCH_SPEED = 30.0
 const ACELERATED_SPEED = 200.0
 const JUMP_VELOCITY = -400.0
-const max_cam_dist = 100.0
-const max_mouse_radius = 500.0
+const max_cam_dist = 170.0
+const max_mouse_radius = 600.0
+const rotation_speed = 10.0
+const min_rotation = -0.5
+const max_rotation = 0.5
+
 
 ## Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -20,29 +28,56 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta):
-	MovementLoop()
+	
+	print(get_global_mouse_position())
+	MovementLoop(delta)
 	move_and_slide()
 
-func MovementLoop() -> void:
+func MovementLoop(delta) -> void:
+	var currentSpeed = SPEED;
 	var direction_y := Input.get_axis("up", "down")
 	var direction_x := Input.get_axis("left", "right")
-	
 	var isRunning = Input.is_action_pressed("run");
-	var currentSpeed = SPEED;
+	var isCrouching = Input.is_action_pressed("crouch");	
+	var isMoving = realVelocity != Vector2.ZERO;
+	print()
 	
-	if (isRunning): 
-		currentSpeed = ACELERATED_SPEED
-	else:
-		currentSpeed = SPEED;
-	
+	match true:
+		isRunning:
+			currentSpeed = ACELERATED_SPEED
+		isCrouching:
+			currentSpeed = CROUCH_SPEED
+			
+		_:
+			currentSpeed = SPEED;
+
 	velocity = Vector2(direction_x, direction_y).normalized() * currentSpeed
 	
 	realVelocity = get_real_velocity()
 	
+	legs.play("walk")
 	if (realVelocity != Vector2.ZERO):
-		$AnimatedSprite2D.play("walk")
+		legs.play("walk")
 	else: 
-		$AnimatedSprite2D.play("idle")
+		legs.play("idle")
+
+# Legs Rotation: 
+	var rotation = 0.0
+
+	if Input.is_action_pressed("left"):
+		rotation = -rotation_speed
+	elif Input.is_action_pressed("right"):
+		rotation = rotation_speed
+
+	rotator.rotation += rotation * delta
+
+	if rotator.rotation < min_rotation:
+		rotator.rotation = min_rotation
+	elif rotator.rotation > max_rotation:
+		rotator.rotation = max_rotation
+
+	if rotation == 0.0:
+		rotator.rotation = 0.0
 	
 	
 func changeCameraZoom(zoom_step: float) -> void:
